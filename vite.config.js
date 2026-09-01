aee79811c76b1bf2c182459066ac38a877e9fcb6
Rodriguez Carlos <Rodriguez15082002@gmail.com>
2026-09-01 03:11:07 -0500

    Activando PWA y Service Worker para modo Offline total

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'] // Esto descarga toda la app en la memoria del celular
+      },
+      manifest: {
+        name: 'Panel Electoral Calima',
+        short_name: 'Electoral',
+        description: 'Gestión territorial de campaña',
+        theme_color: '#0f172a',
+        background_color: '#f1f5f9',
+        display: 'standalone'
+      }
+    })
+  ]
 })
